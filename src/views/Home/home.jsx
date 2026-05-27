9f443e6a6a3815d3488e69936ac323b38f74b3ac
--replace-all <loretodoris68@gmail.com>
2026-05-27 11:18:47 +0200

    feat information-card modificación el home.css yindex.css

--- src/views/Home/home.jsx
+++ src/views/Home/home.jsx
@@ -1,30 +1,30 @@
-    import "./home.css";
-    import { useNavigate } from "react-router-dom";
-    import Button from "../../components/atom/button/button.jsx";
+        import "./home.css";
+        import { useNavigate } from "react-router-dom";
+        import Button from "../../components/atom/button/button.jsx";
 
-    const Home = () => {
-    const navigate = useNavigate();
+        const Home = () => {
+        const navigate = useNavigate();
 
-    return (
-        <div className="home-container">
+        return (
+            <div className="home-container">
 
-      
-        <div className="home-logo">
-            <img src="/src/assets/imagen/logoMovieStick.png" alt="Logo MovieStick" />
-        </div>
+        
+            <div className="home-logo">
+                <img src="/src/assets/imagen/logoMovieStick.png" alt="Logo MovieStick" />
+            </div>
 
 
-       
-        <div className="home-enter-button">
-            <Button 
-            text="Entrar"
-            variant="primary"
-            onClick={() => navigate("/explore")}
-            />
-        </div>
+        
+            <div className="home-enter-button">
+                <Button 
+                text="Entrar"
+                variant="primary"
+                onClick={() => navigate("/explore")}
+                />
+            </div>
 
-        </div>
-    );
-    };
+            </div>
+        );
+        };
 
-    export default Home;
+        export default Home;
